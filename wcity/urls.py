b922d406fe7bc8c22fb13f5039d37494b7aacf58
StatsenkoAnna <anna190711@gmail.com>
2019-04-16 12:02:35 +0300

    шел 7 день джанги, мы выживали как могли

--- wcity/urls.py
+++ wcity/urls.py
@@ -14,10 +14,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.contrib.auth.views import LoginView
 from django.urls import include
 from django.urls import path
 
 urlpatterns = [
-    path('fenster/', include('fenster.urls')),
     path('admin/', admin.site.urls),
+    path('fenster/', include('fenster.urls')),
+    path(
+        'accounts/login/',
+        LoginView.as_view(template_name='fenster/loginpage.html'),
+        name='login'
+    ),
+    path('fenster/', include('fenster.urls')),
 ]
